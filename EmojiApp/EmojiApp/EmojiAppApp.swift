c580554e38ed816231664fb10e1c6af64815a20a
Oriol Angelet <uriaeri@gmail.com>
2024-03-17 18:21:11 +0100

    Lesson 10 (1) Initial commit after creating the Xcode project

---
+++ EmojiApp/EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Uri on 17/3/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
