ff73ceaf384fcf7361819e9122290033dffdd69c
Oriol Angelet <uriaeri@gmail.com>
2024-03-17 18:49:05 +0100

    Lesson 10 First iteration of View Added Extensions from teacher's repo

---
+++ EmojiApp/EmojiApp/Core/Views/EmojiArtDocumentView.swift
@@ -0,0 +1,44 @@
+//
+//  EmojiArtDocumentView.swift
+//  EmojiApp
+//
+//  Created by Uri on 17/3/24.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    
+    private let emojis = "👻🍎😃🤪☹️🤯🐶🐭🦁🐵🦆🐝🐢🐄🐖🌲🌴🌵🍄🌞🌎🔥🌈🌧️🌨️☁️⛄️⛳️🚗🚙🚓🚲🛺🏍️🚘✈️🛩️🚀🚁🏰🏠❤️💤⛵️"
+    
+    var body: some View {
+        VStack {
+            Color.yellow
+            ScrollingEmojis(emojis)
+        }
+    }
+}
+
+#Preview {
+    EmojiArtDocumentView()
+}
+
+struct ScrollingEmojis: View {
+    
+    let emojis: [String]
+    
+    // convert our string of emojis to an array of strings
+    init(_ emojis: String) {
+        self.emojis = emojis.uniqued.map { String($0) }
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                }
+            }
+        }
+    }
+}
--- EmojiApp/EmojiApp/EmojiAppApp.swift
+++ EmojiApp/EmojiApp/EmojiAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EmojiAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            EmojiArtDocumentView()
         }
     }
 }
