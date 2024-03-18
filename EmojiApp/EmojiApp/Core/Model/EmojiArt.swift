cd334cf246dbf20422fd338116e31cd1bb496633
Oriol Angelet <uriaeri@gmail.com>
2024-03-18 18:20:07 +0100

    Lesson 10 Updated model to add emojis and loop with them

--- EmojiApp/EmojiApp/Core/Model/EmojiArt.swift
+++ EmojiApp/EmojiApp/Core/Model/EmojiArt.swift
@@ -9,12 +9,24 @@ import Foundation
 
 struct EmojiArt {
     var background: URL?
-    var emojis = [Emoji]()
+    private(set) var emojis = [Emoji]() // avoids adding emojis outside the model
     
-    struct Emoji {
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(_ emoji: String, at position: Emoji.Position, size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(
+            string: emoji,
+            position: position,
+            size: size,
+            id: uniqueEmojiId))
+    }
+    
+    struct Emoji: Identifiable {
         let string: String
         var position: Position
         var size: Int
+        var id: Int
         
         struct Position {
             var x: Int
