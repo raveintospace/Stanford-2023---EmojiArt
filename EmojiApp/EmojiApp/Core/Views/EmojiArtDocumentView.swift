93d5dadea1b3b8c685f745cd98c5b4510559b735
Oriol Angelet <uriaeri@gmail.com>
2024-03-18 19:18:49 +0100

    Lesson 10 finished Added Sturldata to project Emojis can be dragged & dropped into the documentView

--- EmojiApp/EmojiApp/Core/Views/EmojiArtDocumentView.swift
+++ EmojiApp/EmojiApp/Core/Views/EmojiArtDocumentView.swift
@@ -50,18 +50,37 @@ extension EmojiArtDocumentView {
                         .position(emoji.position.in(geometry))
                 }
             }
-            .dropDestination(for: URL.self) { urls, location in
-                return drop(urls, at: location, in: geometry)
+            .dropDestination(for: Sturldata.self) { sturldatas, location in
+                return drop(sturldatas, at: location, in: geometry)
             }
         }
     }
     
-    private func drop(_ urls: [URL], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
-        if let url = urls.first {
-            document.setBackground(url)
-            return true
+    private func drop(_ sturldatas: [Sturldata], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+        for sturldata in sturldatas {
+            switch sturldata {
+            case .url(let url):
+                document.setBackground(url)
+                return true
+            case .string(let emoji):
+                document.addEmoji(emoji,
+                                  at: emojiPosition(at: location, in: geometry),
+                                  size: paletteEmojiSize
+                )
+                return true
+            default:
+                break
+            }
         }
         return false
+    }
+    
+    private func emojiPosition(at location: CGPoint, in geometry: GeometryProxy) -> Emoji.Position {
+        let center = geometry.frame(in: .local).center
+        return Emoji.Position(
+            x: Int(location.x - center.x),
+            y: Int(-(location.y - center.y))
+            )
     }
 }
 
