743b1abf98a21af9727ac975a2b656f6de603721
Oriol Angelet <uriaeri@gmail.com>
2024-03-23 16:51:57 +0100

    Lesson 12 Updated save method in ViewModel to handle errors with do catch block

--- EmojiApp/EmojiApp/Core/ViewModel/EmojiArtDocument.swift
+++ EmojiApp/EmojiApp/Core/ViewModel/EmojiArtDocument.swift
@@ -26,8 +26,12 @@ class EmojiArtDocument: ObservableObject {
     }
     
     private func save(to url: URL) {
-        let data = emojiArt.json()
-        data.write(to: url)
+        do {
+            let data = try emojiArt.json()
+            try data.write(to: url)
+        } catch let error {
+            debugPrint("EmojiArtDocument: error while autosaving \(error.localizedDescription)")
+        }
     }
     
     init() {
