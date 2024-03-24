ba87a071883e5628dc57b279caa1f85f61d3e7b8
Oriol Angelet <uriaeri@gmail.com>
2024-03-24 13:50:47 +0100

    Lesson 13 PaletteView navigates to a View where each emoji is shown individually with size 300

--- EmojiApp/EmojiApp/Core/Views/PaletteView.swift
+++ EmojiApp/EmojiApp/Core/Views/PaletteView.swift
@@ -11,12 +11,20 @@ struct PaletteView: View {
     
     let palette: Palette
     
+    private let singleEmojiFont: Font = Font.system(size: 300)
+    
     var body: some View {
         VStack {
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
                 ForEach(palette.emojis.uniqued.map(String.init), id: \.self) { emoji in
-                    Text(emoji)
+                    NavigationLink(value: emoji) {
+                        Text(emoji)
+                    }
                 }
+            }
+            .navigationDestination(for: String.self) { emoji in
+                Text(emoji)
+                    .font(singleEmojiFont)
             }
             Spacer()
         }
