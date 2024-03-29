149a7ca59d14b2c6c4749b59156b3f9492ce3fe1
Oriol Angelet <uriaeri@gmail.com>
2024-03-29 16:16:35 +0100

    AnimatedActionButton moved to its own file

---
+++ EmojiApp/EmojiApp/Core/Components/AnimatedActionButton.swift
@@ -0,0 +1,43 @@
+//
+//  AnimatedActionButton.swift
+//  EmojiApp
+//
+//  Created by Uri on 29/3/24.
+//
+
+import SwiftUI
+
+struct AnimatedActionButton: View {
+    var title: String? = nil
+    var systemImage: String? = nil
+    var role: ButtonRole?
+    let action: () -> Void
+    
+    init(_ title: String? = nil,
+         systemImage: String? = nil,
+         role: ButtonRole? = nil,
+         action: @escaping () -> Void   // called after the function it was passed to returns
+    ) {
+        self.title = title
+        self.systemImage = systemImage
+        self.role = role
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(role: role) {
+            withAnimation {
+                action()
+            }
+        } label: {
+            if let title, let systemImage {
+                Label(title, systemImage: systemImage)
+            } else if let title {
+                Text(title)
+            } else if let systemImage {
+                Image(systemName: systemImage)
+            }
+        }
+    }
+}
+
--- EmojiApp/EmojiApp/Extensions/Extensions.swift
+++ EmojiApp/EmojiApp/Extensions/Extensions.swift
@@ -52,40 +52,6 @@ extension AnyTransition {
     static let rollDown: AnyTransition = .asymmetric(insertion: .move(edge: .top), removal: .move(edge: .bottom))
 }
 
-struct AnimatedActionButton: View {
-    var title: String? = nil
-    var systemImage: String? = nil
-    var role: ButtonRole?
-    let action: () -> Void
-    
-    init(_ title: String? = nil,
-         systemImage: String? = nil,
-         role: ButtonRole? = nil,
-         action: @escaping () -> Void   // called after the function it was passed to returns
-    ) {
-        self.title = title
-        self.systemImage = systemImage
-        self.role = role
-        self.action = action
-    }
-    
-    var body: some View {
-        Button(role: role) {
-            withAnimation {
-                action()
-            }
-        } label: {
-            if let title, let systemImage {
-                Label(title, systemImage: systemImage)
-            } else if let title {
-                Text(title)
-            } else if let systemImage {
-                Image(systemName: systemImage)
-            }
-        }
-    }
-}
-
 extension Character {
     var isEmoji: Bool {
         // Swift does not have a way to ask if a Character isEmoji
